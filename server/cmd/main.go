02f67de241fd6ba357dcc2f38758e92701e3a562
Ainur Ibatov <me@ibatov.ru>
2022-11-15 10:48:57 +0300

    add vault and fix build

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -9,11 +9,15 @@ import (
 )
 
 var (
-	configPath string
+	configPath   string
+	psqlPassword string
+	psqlUser     string
 )
 
 func init() {
 	flag.StringVar(&configPath, "config-path", "configs/config.toml", "path to config file")
+	flag.StringVar(&psqlPassword, "psql-password", "", "password for psql auth")
+	flag.StringVar(&psqlUser, "psql-user", "", "user for psql auth")
 }
 
 // @title EMKN API
@@ -21,7 +25,7 @@ func init() {
 // @description This is a backend for EMKN app
 // @contact.name API Support
 // @contact.email https://t.me/Intellec2aI
-// @host localhost:8080
+// @host 51.250.98.212:8080
 // @BasePath /
 func main() {
 	flag.Parse()
@@ -32,6 +36,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	config.StorageConfig.Auth(psqlUser, psqlPassword)
 
 	s := core.New(config)
 	if err := s.Start(); err != nil {
--- server/internal/app/storage/config.go
+++ server/internal/app/storage/config.go
@@ -4,6 +4,10 @@ type Config struct {
 	DatabaseURL string `toml:"database_url"`
 }
 
+func (c *Config) Auth(user string, password string) {
+	c.DatabaseURL += " user=" + user + " password=" + password
+}
+
 func NewConfig() *Config {
 	return &Config{}
 }

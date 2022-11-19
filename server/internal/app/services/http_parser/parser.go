25c677e6ae65f231043888105c7d9f5cbdadb350
Ainur Ibatov <me@ibatov.ru>
2022-11-19 17:02:10 +0300

    Revert "fix bugs"
    This reverts commit 79b8308a57896afd17185de4297ec007ab4d9df1.

--- server/internal/app/services/http_parser/parser.go
+++ server/internal/app/services/http_parser/parser.go
@@ -2,23 +2,17 @@ package http_parser
 
 import (
 	"encoding/json"
-	"github.com/sava-cska/SPbSU-EMKN/internal/app/core/dependency"
 	"io"
 	"net/http"
 )
 
-func ParseBody(value any, request *http.Request, context *dependency.DependencyContext) error {
+func ParseBody(value any, request *http.Request) error {
 	data, errReq := io.ReadAll(request.Body)
-	if errReq == io.EOF {
-		return nil
-	}
 	if errReq != nil {
-		context.Logger.Errorf("ParseBody readall error: %s", errReq)
 		return errReq
 	}
 	if errJSON := json.Unmarshal(data, value); errJSON != nil {
-		context.Logger.Errorf("ParseBody unmarshal error: %s", errJSON)
-		return nil
+		return errJSON
 	}
 	return nil
 }
